2e4542392e628993ec387b22b6a88734e939a445
Zablon5 <zgirma74@gmail.com>
2022-12-08 12:37:04 -0800

    edited a file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - entry
- * return - 0
- */
+* main - entry
+* return - 0
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
