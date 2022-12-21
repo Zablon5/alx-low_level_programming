31c83d6800d054ba47491216ff7824a11434ef48
Zablon5 <zgirma74@gmail.com>
2022-12-21 11:08:11 -0800

    added 7 projects

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: strint 2
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+	while (*s1 != '\0')
+	{
+		if (*s2 == '\0')
+			return (*s1);
+		if (*s2 > *s1)
+			return (*s1 - *s2);
+		if (*s1 > *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s2 != '\0')
+		return (*s2);
+	return (0);
+}
