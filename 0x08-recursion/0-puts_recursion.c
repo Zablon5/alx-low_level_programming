2e2f286c9ef75ab6be779f06c927d88749f1e962
Zerihun Girma <zgirma74@gmail.com>
2023-01-04 05:38:54 -0800

    added 3 files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string recursively
+ * @s: pointer to string
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_puts_recursion(s+1);
+}
