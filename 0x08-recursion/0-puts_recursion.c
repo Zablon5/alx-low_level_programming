2a14331082fafe98dee958a56514ab92791f3d58
Zerihun Girma <zgirma74@gmail.com>
2023-01-04 05:45:23 -0800

    edited a file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	else
 	{
 		_putchar('\n');
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
