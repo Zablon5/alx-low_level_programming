ccf89d2be89875d8325040069d8703268ee69357
Zerihun Girma <zgirma74@gmail.com>
2023-01-04 08:48:42 -0800

    added a factorial calculator file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial-returns a factorial of a given number
+ * @n:integer input
+ * Return: integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
