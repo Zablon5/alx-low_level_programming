2e287bd14a7bbc66d8593749ae63eb2dafe31bc7
Zerihun Girma <99526925+Zablon5@users.noreply.github.com>
2023-01-11 23:58:42 +0300

    added a string copier file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	copy = malloc(sizeof(char) * (len + 1));
+
+	if (copy == NULL)
+		return (NULL);
+
+	while ((copy[i] = str[i]) != '\0')
+		i++;
+
+	return (copy);
+}
