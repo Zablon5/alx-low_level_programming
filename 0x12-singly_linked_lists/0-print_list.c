47e84040c55cefd621d86308e4b10692ef70430b
Zerihun Girma <zgirma74@gmail.com>
2023-01-27 12:38:22 -0800

    added the advanced tasks plus edited the first task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,13 +3,13 @@
 
 /**
  * print_list - prints elements of a list_t list.
- * @h: The list_t list.
+ * @h: pointer to the list_t list.
  * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
 {
-	register int len = 0;
+	size_t len = 0;
 
 	while (h)
 	{
