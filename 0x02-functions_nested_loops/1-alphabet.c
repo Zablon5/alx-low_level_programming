79c40a634994e1c676fb546abff9bd400e264576
Zerihun Girma <zgirma74@gmail.com>
2023-02-27 13:49:46 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,13 @@
  */
 void print_alphabet(void)
 {
-    char letter = 'a';
+char letter = 'a';
 
-    while (letter <= 'z')
-    {
-        _putchar(letter);
-        letter++;
-    }
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
 
-    _putchar('\n');
+_putchar('\n');
 }
