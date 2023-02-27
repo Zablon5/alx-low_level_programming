f5af13a0536324fb40498bdbbd85f80db5248428
Zerihun Girma <zgirma74@gmail.com>
2023-02-27 17:05:34 +0300

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+int ans=0,i=0;
+while(i<1024)
+{
+if(i%3==0 || i%5==0)
+{
+
+ans+=i;
+        
+}
+i++;
+}
+printf("%d\n",ans);
+return (0);
+
+}
