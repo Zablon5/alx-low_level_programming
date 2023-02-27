4982801feff1a32ac76a18bec6a048358e762f51
Zerihun Girma <zgirma74@gmail.com>
2023-02-27 16:22:40 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,14 +8,14 @@
  */
 void print_to_98(int n)
 {
-    while (n != 98)
-    {
-        printf("%d, ", n);
-        if (n > 98)
-            n--;
-        else
-            n++;
-    }
-    printf("98\n");
+while (n != 98)
+{
+printf("%d, ", n);
+if (n > 98)
+n--;
+else
+n++;
+}
+printf("98\n");
 }
 
