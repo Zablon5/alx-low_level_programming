d38f2b8168def02d743c9f9117ffa62c84287e45
Zerihun Girma <zgirma74@gmail.com>
2023-02-27 15:20:48 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,13 +8,13 @@
  */
 int _abs(int n)
 {
-    if (n < 0)
-    {
-        return (-n);
-    }
-    else
-    {
-        return (n);
-    }
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
 }
 
