9fe8a1eb5a5a8777505ad2c4d169ef294157cbe9
Zerihun Girma <zgirma74@gmail.com>
2023-02-27 17:11:20 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,14 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 int ans = 0, i = 0;
 while (i < 1024)
 {
 if (i % 3 == 0 || i % 5 == 0)
 {
-ans += i;      
+ans += i;
 }
 i++;
 }
