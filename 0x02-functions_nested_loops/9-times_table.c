f1976352d237d29e11f72547db6f6d07d5fc9d48
Zerihun Girma <zgirma74@gmail.com>
2023-02-27 16:12:11 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,37 +5,37 @@
  *
  * Return: void
  */
-void times_table(void)
+void time_table(void)
 {
-    int i = 0, j = 0, product;
+int i = 0, j = 0, product;
 
-    while (i <= 9)
-    {
-        j = 0;
-        while (j <= 9)
-        {
-            product = i * j;
-            if (j == 0)
-                _putchar('0');
-            else if (product < 10)
-            {
-                _putchar(' ');
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-            if (j < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            j++;
-        }
-        _putchar('\n');
-        i++;
-    }
+while (i <= 9)
+{
+j = 0;
+while (j <= 9)
+{
+product = i * j;
+if (j == 0)
+_putchar('0');
+else if (product < 10)
+{
+_putchar(' ');
+_putchar(product + '0');
+}
+else
+{
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+j++;
+}
+_putchar('\n');
+i++;
+}
 }
 
