6a6b3a9027fd6e7721546c501d886f792a0b2da7
Zerihun Girma <zgirma74@gmail.com>
2023-02-27 13:58:38 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,21 +7,19 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter = 'a';
-    int i = 0;
+char letter = 'a';
+int i = 0;
 
-    while (i < 10)
-    {
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-        letter = 'a';
-        i++;
-    }
-
-    
+while (i < 10)
+{
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+letter = 'a';
+i++;
+}
 }
 
