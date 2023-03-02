29db3bb910beabfba06aa4a2558447259f6b4883
Zerihun Girma <zgirma74@gmail.com>
2023-03-02 10:32:25 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* puts2-prints every other character of a string, starting 
+* puts2-prints every other character of a string, starting
 * with the first character, followed by a new line
 * @str:reference to string input
 * Return:void
