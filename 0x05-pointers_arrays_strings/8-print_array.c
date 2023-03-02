eb2b98d378ea5dc4a3de763438aec35be470f391
Zerihun Girma <zgirma74@gmail.com>
2023-03-02 11:32:11 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,10 +5,16 @@
 * @a:reference to an array of integers
 * @n:number of elements of the array to be printed
 * Return:void
-*/
+*
 void print_array(int *a, int n)
 {
 int i = 0;
+if (n<=0)
+{
+printf("\n");
+}
+else
+{
 while (i < n - 1)
 {
 printf("%d, ", a[i]);
@@ -16,4 +22,5 @@ i++;
 }
 printf("%d", a[i]);
 printf("\n");
+}
 }
