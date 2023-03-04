08490d6a492a99800db1efa41928620779d563a5
Zerihun Girma <zgirma74@gmail.com>
2023-03-04 21:30:02 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,5 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-
-	while (*s1 != '\0')
-	{
-		if (*s2 == '\0')
-			return (*s1);
-		if (*s2 > *s1)
-			return (*s1 - *s2);
-		if (*s1 > *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-	}
-	if (*s2 != '\0')
-		return (*s2);
-	return (0);
+return (s1[0] - s2[0]);
 }
