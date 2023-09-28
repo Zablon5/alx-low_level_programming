bbea15c94c75d3d2b72e792198ced6fe6d85e72a
Zerihun Girma <zgirma74@gmail.com>
2023-09-28 12:32:45 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen_recursion - returns the length of a string
- * @s:ponter to a string
+ * @s:ponter to string
  * Return: integer
  */
 int _strlen_recursion(char *s)
